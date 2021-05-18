251b435e94effc9de70b643e6b8264d96f817744
Ilya <37418897+IlyaSP@users.noreply.github.com>
2021-05-18 14:22:39 +0300

    Update restore_docker_volume_backup.py

--- docker/restore_docker_volume_backup.py
+++ docker/restore_docker_volume_backup.py
@@ -1,55 +1,88 @@
 import os
-import paramiko
-import scp
+import re
+import sys
+import subprocess
 import tarfile
-import glob
 
 
-def get_list_docker_volume():
-   """Получение списка и точек монтирования Volume"""
+def get_list_backup_volume(dir):
+    """Получения списка volume из архивных файлов"""
+    backup_volume = []
+    dict_list_arhive = {}
+    # print(os.listdir(dir))
+    for i in os.listdir(dir):
+        if re.search(r'tar', i) is not None:
+            dict_list_arhive[i.split('.')[0]] = i
+            backup_volume.append(i.split('.')[0])
 
-   stream = os.popen('docker volume ls --format "{{.Name}}: {{.Mountpoint}}"')
-   dict_volume_list = {}
-   for i in stream.readlines():
-      new_element = i.replace('\n','').split(':')
-      dict_volume_list[new_element[0].strip()] = new_element[1].strip()
-
-   return (dict_volume_list)
-
-
-def create_backup_volume(dict_volume_list):
-    """Создание архивов volume"""
-    for vol, dir in dict_volume_list.items():
-      print('Start backup volume {0}'.format(vol))
-      tar_name = '{0}.tar'.format(vol)
-      with tarfile.open(tar_name, 'x') as tar:    # Архив будет создан, только в случае, если его нет в данной директории
-         for i in os.listdir(dir):
-            print('Add file/directory "{0}" to archive'.format(i))
-            tar.add('{0}/{1}'.format(dir, i), arcname=i)
+    return dict_list_arhive, backup_volume
 
 
-def copy_backup_to_server(server, port, user, password):
-   """Копирование архивов с бэкапом вольюмов на удалённый сервер"""
+def get_list_exist_volume():
+    exist_volume = []
+    stream = os.popen('docker volume ls --format "{{.Name}}"')
+    vol = stream.readlines()
+    if len(vol) != 0:
+        for i in vol:
+            exist_volume.append(i.replace('\n','').strip())
+    return exist_volume
 
-   ssh = paramiko.SSHClient()
-   ssh.load_system_host_keys()
-   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-   ssh.connect(server, port, user, password)
 
-   # SCPCLient takes a paramiko transport as an argument
-   scp_connect = scp.SCPClient(ssh.get_transport())
-   files_to_copy = (glob.glob('*.tar'))
-   for file in files_to_copy:
-      print('Copy "{0}" to {1}'.format(file, server))
-      scp_connect.put(file, remote_path='/home/{0}/backup_vol/{1}'.format(user, file))
-      #scp_connect.put(file)
+def check_intersection_volume(list_backup_volume, list_exist_volume):
+    if len(set(list_backup_volume).intersection(set(list_exist_volume))) != 0:
+        status = 1
+    else:
+        status = 0
+    return status
 
-   scp_connect.close()
-   ssh.close()
+
+def create_volume(list_backup_volume):
+    for vol in list_backup_volume:
+        command = subprocess.run(['docker', 'volume', 'create' , vol], stdout=subprocess.PIPE,  encoding='utf-8')
+        if command.returncode != 0:
+            print("Error create {0} volume".format(vol))
+            sys.exit(command.stderr)
+        else:
+            continue
+
+def get_list_all_exist_volume():
+    stream = os.popen('docker volume ls --format "{{.Name}}: {{.Mountpoint}}"')
+    dict_volume_list = {}
+    for i in stream.readlines():
+        new_element = i.replace('\n', '').split(':')
+        dict_volume_list[new_element[0].strip()] = new_element[1].strip()
+
+    return dict_volume_list
+
+
+def extract_backup_to_vol(list_backup_volume, dict_volume_list, dict_list_arhive, dir):
+    for vol in list_backup_volume:
+        dir_mount = dict_volume_list.get(vol)
+        # print(dir_mount)
+        if dir_mount is None:
+            sys.exit("No volume")
+        else:
+            print('Extcract volume {0}'.format(vol))
+            tar_file = '{0}/{1}'.format(dir, dict_list_arhive.get(vol))
+            with tarfile.open(tar_file, 'r') as tar:
+                tar.extractall(path=dir_mount)
+
 
 if __name__ == '__main__':
-   path_temp_folder = '/tmp_folder_docker_volume'
-   dict_volume_list = get_list_docker_volume()
-   print(dict_volume_list)
-   create_backup_volume(dict_volume_list)
-   copy_backup_to_server(server="192.168.108.132", port=22, user="bobr", password="12345678")
+
+    dict_list_arhive, list_backup_volume = get_list_backup_volume(dir='.')
+    print(list_backup_volume)
+
+    list_exist_volume = get_list_exist_volume()
+    print(list_exist_volume)
+
+    status = check_intersection_volume(list_backup_volume, list_exist_volume)
+    if status != 0:
+        sys.exit('there is an intersection of backup and exist volume')
+    else:
+        create_volume(list_backup_volume)
+
+    dict_volume_list = get_list_all_exist_volume()
+    print(dict_volume_list)
+
+    extract_backup_to_vol(list_backup_volume, dict_volume_list, dict_list_arhive, dir='.')
